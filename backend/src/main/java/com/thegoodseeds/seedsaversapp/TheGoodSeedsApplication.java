db2b0f28be958741776fa74cd792a0e937a45000
Roberta Brasil <robertabrasil.dev@gmail.com>
2022-07-26 22:14:55 -0300

    Project created

---
+++ backend/src/main/java/com/thegoodseeds/seedsaversapp/TheGoodSeedsApplication.java
@@ -0,0 +1,13 @@
+package com.thegoodseeds.seedsaversapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGoodSeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGoodSeedsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thegoodseeds/seedsaversapp/TheGoodSeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegoodseeds.seedsaversapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGoodSeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
