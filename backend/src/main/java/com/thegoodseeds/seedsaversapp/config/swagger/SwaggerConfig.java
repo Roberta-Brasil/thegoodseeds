aeb20cc6e15038525fe31be164b156b4512e37bd
Roberta Brasil <robertabrasil.dev@gmail.com>
2022-08-04 01:40:12 -0300

    Swagger UI documentation created

---
+++ backend/src/main/java/com/thegoodseeds/seedsaversapp/config/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.thegoodseeds.seedsaversapp.config.swagger;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+     GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("robertaApi")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI apiAlmada() {
+        return new OpenAPI()
+                .info(new Info().title("The Good Seeds API")
+                        .description("Thesis project of the API of Roberta")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+	
+
+}
