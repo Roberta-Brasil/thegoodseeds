e7f68a274646015fd244d6b60cd348b317c9f91b
Roberta Brasil <robertabrasil.dev@gmail.com>
2022-08-04 01:33:53 -0300

    Refactoring end-points Post & Comment

--- backend/src/main/java/com/thegoodseeds/seedsaversapp/controllers/PostController.java
+++ backend/src/main/java/com/thegoodseeds/seedsaversapp/controllers/PostController.java
@@ -1,35 +1,58 @@
 package com.thegoodseeds.seedsaversapp.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import com.thegoodseeds.seedsaversapp.entities.Post;
-import com.thegoodseeds.seedsaversapp.entities.dtos.PostRequestDTO;
-import com.thegoodseeds.seedsaversapp.repositories.PostRepository;
+import com.thegoodseeds.seedsaversapp.dtos.request.PostRequestDTO;
+import com.thegoodseeds.seedsaversapp.dtos.response.PostResponseDto;
+import com.thegoodseeds.seedsaversapp.service.PostService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping(value= "/post")
 public class PostController {
 	
 	@Autowired
-	PostRepository postRepo;
+	private PostService postService;
 	
 	@GetMapping
-	public ResponseEntity<List<Post>> findAll() {
-		return ResponseEntity.status(HttpStatus.OK).body(postRepo.findAll());
+	@Operation(summary= "Find all posts in the System.")
+	public ResponseEntity<List<PostResponseDto>> findAll() {
+		
+		List<PostResponseDto> postsDto = postService.findAll();
+	
+		return ResponseEntity.ok().body(postsDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	@Operation(summary= "Find a specific post in the System by id")
+	public ResponseEntity<PostResponseDto> findById(@PathVariable Long id) {
+		
+		PostResponseDto postDto = postService.findById(id);
+		
+		return ResponseEntity.ok().body(postDto);
 	}
 
 	@PostMapping
-	public ResponseEntity<Post> insert(@RequestBody PostRequestDTO obj) {
-		Post result = postRepo.save(new Post(obj.getLikesQuantity(), obj.getTitle(), obj.getPostMessage()));
-		return ResponseEntity.status(HttpStatus.CREATED).body(result);
+	@Operation(summary= "Insert a post in the system.")
+	public ResponseEntity<PostResponseDto> insert(@RequestBody PostRequestDTO obj, UriComponentsBuilder uriBuilder) {
+		
+		PostResponseDto postDto = postService.insert(obj); 
+		
+		
+		URI uri = uriBuilder.path("/post/{id}").buildAndExpand(postDto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(postDto);
 	}
 }
